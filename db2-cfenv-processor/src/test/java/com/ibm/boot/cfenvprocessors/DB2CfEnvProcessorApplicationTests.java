2549cd848a1cc99c835494afbe8d14669f45e921
George Foster <george.foster@ibm.com>
2019-11-06 17:41:52 -0800

    Refactor the test directories

---
+++ db2-cfenv-processor/src/test/java/com/ibm/boot/cfenvprocessors/DB2CfEnvProcessorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ibm.boot.cfenvprocessors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class DB2CfEnvProcessorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ watson-natural-language-understanding-cfenv-processor/src/test/java/com/ibm/boot/cfenvprocessors/WatsonNaturalLanguageUnderstandingCfEnvProcessorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ibm.boot.cfenvprocessors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class WatsonNaturalLanguageUnderstandingCfEnvProcessorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
