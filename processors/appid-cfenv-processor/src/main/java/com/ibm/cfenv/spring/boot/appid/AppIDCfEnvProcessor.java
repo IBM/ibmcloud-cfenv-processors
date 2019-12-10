59e58de391c879e2f36a82dab0660f49c26f9273
Harish Yayi <yharish991@gmail.com>
2019-12-10 11:39:29 -0500

    add cfenv processor for appid

---
+++ processors/appid-cfenv-processor/src/main/java/com/ibm/cfenv/spring/boot/appid/AppIDCfEnvProcessor.java
@@ -0,0 +1,37 @@
+package com.ibm.cfenv.spring.boot.appid;
+
+import io.pivotal.cfenv.core.CfCredentials;
+import io.pivotal.cfenv.core.CfService;
+import io.pivotal.cfenv.spring.boot.CfEnvProcessor;
+import io.pivotal.cfenv.spring.boot.CfEnvProcessorProperties;
+
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class AppIDCfEnvProcessor implements CfEnvProcessor {
+
+    private static final Logger LOG = Logger.getLogger(AppIDCfEnvProcessor.class.getName());
+
+    @Override
+    public boolean accept(CfService service) {
+        boolean match = service.existsByLabelStartsWith("AppID");
+        LOG.info("AppIDCfEnvProcessor matched service entry : " + service.getName());
+        return match;
+    }
+
+    @Override
+    public CfEnvProcessorProperties getProperties() {
+        return CfEnvProcessorProperties.builder()
+                .propertyPrefixes("spring.security.oauth2.client.registration.appid")
+                .serviceName("AppID")
+                .build();
+    }
+
+    @Override
+    public void process(CfCredentials cfCredentials, Map<String, Object> properties) {
+        Map<String, Object> credentials = cfCredentials.getMap();
+        properties.put("spring.security.oauth2.client.registration.appid.clientId", credentials.get("clientId"));
+        properties.put("spring.security.oauth2.client.registration.appid.clientSecret", credentials.get("secret"));
+        properties.put("spring.security.oauth2.client.registration.appid.issuerUri", credentials.get("discoveryEndpoint"));
+    }
+}
---
+++ processors/appid-cfenv-processor/src/test/java/com/ibm/cfenv/spring/boot/appid/AppIDCfEnvProcessorTest.java
@@ -0,0 +1,58 @@
+package com.ibm.cfenv.spring.boot.appid;
+
+import io.pivotal.cfenv.core.CfService;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.cfenv.spring.boot.appid.AppIDCfEnvProcessor;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AppIDCfEnvProcessorTest {
+
+    AppIDCfEnvProcessor appIDCfEnvProcessor = new AppIDCfEnvProcessor();
+    private CfService service;
+    
+    @BeforeAll
+    public void initService() throws IOException {
+        URL url = AppIDCfEnvProcessor.class.getClassLoader().getResource("./vcap-services.json");
+        Map<String, Object> serviceData = (Map<String, Object>) ((List) new ObjectMapper()
+                .readValue(url, Map.class)
+                .get("AppID"))
+                .get(0);
+        System.out.println(serviceData);
+        service = new CfService(serviceData);
+    }
+    
+    @Test
+    public void accept() {
+        assertThat(appIDCfEnvProcessor.accept(service)).isTrue();
+    }
+
+    @Test
+    public void process() {
+        Map<String, Object> properties = new HashMap<>();
+        appIDCfEnvProcessor.process(service.getCredentials(), properties);
+        assertThat(properties.size()).isEqualTo(3);
+        assertThat(properties.get("spring.security.oauth2.client.registration.appid.clientId")).isEqualTo("appid_clientId");
+        assertThat(properties.get("spring.security.oauth2.client.registration.appid.clientSecret")).isEqualTo("appid_secret");
+        assertThat(properties.get("spring.security.oauth2.client.registration.appid.issuerUri")).isEqualTo("appid_discoveryEndpoint");
+    }
+
+    @Test
+    public void getProperties() {
+        assertThat(appIDCfEnvProcessor.getProperties().getServiceName()).isEqualTo("AppID");
+        assertThat(appIDCfEnvProcessor.getProperties().getPropertyPrefixes()).isEqualTo("spring.security.oauth2.client.registration.appid");
+
+    }
+}
